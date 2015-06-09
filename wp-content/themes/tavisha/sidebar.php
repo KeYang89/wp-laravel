5ad61f97392da1c62a66e0240c54e8e1ab48c853
Ke Yang <key@goretailpro.com>
2015-06-09 15:54:36 -0500

    Modify the fucntions.php

--- wp-content/themes/tavisha/sidebar.php
+++ wp-content/themes/tavisha/sidebar.php
@@ -1,6 +1,42 @@
 <?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
   <div id="widget-area" class="sidebar column col3 col4-sm" role="complementary">
     <?php dynamic_sidebar( 'sidebar-1' ); ?>
+<div class="login-box-body" style="background-color:rgba(255,255,255,0.5);padding:10px;">
+        <h3>Sign/Register</h3>
+        <form role="form" method="POST" action="http://matchday45.com/team/auth/login">
+            <input type="hidden" name="_token" value="<?php ?>">
+            <div class="form-group has-feedback">
+                <input type="email" class="form-control" placeholder="Email" name="email" value="" autocomplete="off" style="cursor: auto;">
+                <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+            </div>
+            <div class="form-group has-feedback">
+                <input type="password" class="form-control" placeholder="Password" name="password" autocomplete="off">
+                <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+            </div>
+            <div class="row">
+                <div class="col-xs-8">
+                    <div class="checkbox icheck">
+                        <label>
+                            <div class="icheckbox_square-blue checked" aria-checked="false" aria-disabled="false" style="position: relative;"><input type="checkbox" name="remember" checked="" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"><ins class="iCheck-helper" style="position: absolute; top: -20%; left: -20%; display: block; width: 140%; height: 140%; margin: 0px; padding: 0px; border: 0px; opacity: 0; background: rgb(255, 255, 255);"></ins></div> Remember Me
+                        </label>
+                    </div>
+                </div><!-- /.col -->
+                <div class="col-xs-4">
+                    <button type="submit" class="btn btn-primary btn-block btn-flat btn-black">Sign In</button>
+                </div><!-- /.col -->
+            </div>
+        </form>
+
+        <div class="social-auth-links text-center">
+            <p>- OR -</p>
+            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
+            <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
+        </div><!-- /.social-auth-links -->
+
+        <a href="#">I forgot my password</a><br>
+        <a href="http://matchday45.com/team/auth/register" class="text-center">Register a new membership</a>
+
+    </div>
 <div class="team">
                              <div class="team-header">
                              
@@ -10,12 +46,12 @@
                              <div class="teambox">
                              <a href="#"><div class="btn1">
                              
-                             	Join League
+                                Join League
                                 
                              </div></a>
                              <a href="#"><div class="btn2">
                              
-                             	Create League
+                                Create League
                                 
                              </div></a>
                              
