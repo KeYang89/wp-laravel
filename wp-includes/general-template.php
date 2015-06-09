548e274b0ed02c1b37f6825a59f99f91cedf3eb3
SiD <creativemirage@live.in>
2015-06-09 23:34:39 +0530

    curl check

--- wp-includes/general-template.php
+++ wp-includes/general-template.php
@@ -450,7 +450,8 @@ function wp_login_form( $args = array() ) {
 	 */
 	$login_form_bottom = apply_filters( 'login_form_bottom', '', $args );
 
-	$form = '
+    /*
+    $form = '
 		<form name="' . $args['form_id'] . '" id="' . $args['form_id'] . '" action="' . esc_url( site_url( 'wp-login.php', 'login_post' ) ) . '" method="post">
 			' . $login_form_top . '
 			<p class="login-username">
@@ -460,6 +461,28 @@ function wp_login_form( $args = array() ) {
 			<p class="login-password">
 				<label for="' . esc_attr( $args['id_password'] ) . '">' . esc_html( $args['label_password'] ) . '</label>
 				<input type="password" name="pwd" id="' . esc_attr( $args['id_password'] ) . '" class="input" value="" size="20" />
+			</p>
+			' . $login_form_middle . '
+			' . ( $args['remember'] ? '<p class="login-remember"><label><input name="rememberme" type="checkbox" id="' . esc_attr( $args['id_remember'] ) . '" value="forever"' . ( $args['value_remember'] ? ' checked="checked"' : '' ) . ' /> ' . esc_html( $args['label_remember'] ) . '</label></p>' : '' ) . '
+			<p class="login-submit">
+				<input type="submit" name="wp-submit" id="' . esc_attr( $args['id_submit'] ) . '" class="button-primary" value="' . esc_attr( $args['label_log_in'] ) . '" />
+				<input type="hidden" name="redirect_to" value="' . esc_url( $args['redirect'] ) . '" />
+			</p>
+			' . $login_form_bottom . '
+		</form>';
+    */
+	$form = '
+		<form name="' . $args['form_id'] . '" id="' . $args['form_id'] . '" action="/team/auth/login" method="post">
+		    <input type="hidden" value="' . getToken() . '" name="_token">
+
+			' . $login_form_top . '
+			<p class="login-username">
+				<label for="' . esc_attr( $args['id_username'] ) . '">' . esc_html( $args['label_username'] ) . '</label>
+				<input type="text" name="email" id="' . esc_attr( $args['id_username'] ) . '" class="input" value="" size="20" />
+			</p>
+			<p class="login-password">
+				<label for="' . esc_attr( $args['id_password'] ) . '">' . esc_html( $args['label_password'] ) . '</label>
+				<input type="password" name="password" id="' . esc_attr( $args['id_password'] ) . '" class="input" value="" size="20" />
 			</p>
 			' . $login_form_middle . '
 			' . ( $args['remember'] ? '<p class="login-remember"><label><input name="rememberme" type="checkbox" id="' . esc_attr( $args['id_remember'] ) . '" value="forever"' . ( $args['value_remember'] ? ' checked="checked"' : '' ) . ' /> ' . esc_html( $args['label_remember'] ) . '</label></p>' : '' ) . '
@@ -3156,4 +3179,20 @@ function wp_heartbeat_settings( $settings ) {
 		$settings['nonce'] = wp_create_nonce( 'heartbeat-nonce' );
 
 	return $settings;
+}
+
+
+function getToken()
+{
+    $ch = curl_init();
+
+    curl_setopt($ch,CURLOPT_URL, 'http://matchday45.com/team/api/v1/getToken');
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+//  curl_setopt($ch,CURLOPT_HEADER, false);
+
+    $output=curl_exec($ch);
+
+    curl_close($ch);
+    unset($ch);
+    return $output;
 }
