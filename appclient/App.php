278ee82689b6c3b386c8b50bd439d50dc605744d
SiD <creativemirage@live.in>
2015-06-13 18:13:09 +0530

    Curl Test

--- appclient/App.php
+++ appclient/App.php
@@ -19,7 +19,7 @@ class App {
     public static function apiCall($endpoint)
     {
         $ch = curl_init();
-        curl_setopt($ch,CURLOPT_URL, 'http://localhost/team/api/v1/' . $endpoint);
+        curl_setopt($ch,CURLOPT_URL, 'http://matchday45.com/team/api/v1/' . $endpoint);
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
         //curl_setopt($ch,CURLOPT_HEADER, false);
         $output=curl_exec($ch);
