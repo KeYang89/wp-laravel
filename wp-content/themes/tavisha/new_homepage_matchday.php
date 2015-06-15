92d4e1071d302724e92f70bfe8d651f7a4e69b6c
Ke Yang <key@goretailpro.com>
2015-06-15 18:22:52 -0500

    homepage design change

--- wp-content/themes/tavisha/new_homepage_matchday.php
+++ wp-content/themes/tavisha/new_homepage_matchday.php
@@ -10,7 +10,7 @@ get_header();
 
   <div class="col3">
 
-  <img class="alignnone size-full wp-image-50" src="/wp-content/uploads/2014/08/daily-fantasy.png" alt="daily-fantasy" width="880" height="692"></div>
+  <img class="alignnone size-full wp-image-50" src="/wp-content/uploads/2015/06/daily-fantasy.png" alt="daily-fantasy" width="880" height="692"></div>
 <div class="col5">
 <h2>DAILY FANTASY</h2>
 <h3>An entire fantasy season in one day!</h3>
@@ -37,37 +37,37 @@ get_header();
 
 
 <div id="homefeatures" class="avia-section main_color avia-section-default avia-no-shadow avia-bg-style-scroll  avia-builder-el-9  el_after_av_section  el_before_av_section  container_wrap fullsize" style="background-color:#E3B013;" data-section-bg-repeat="repeat">
-<div>
+
 <h2><span class="ss-icon ss-air ss-toggles">&nbsp;</span>FEATURES</h2>
 <h3>Stats and info that actually matter to you.</h3>
 
 <p></p><div class="flex_column av_one_third first  "><div style="height:30px" class="hr hr-invisible "><span class="hr-inner"><span class="hr-inner-style"></span></span></div><img class="alignnone size-full wp-image-63" src="https://mondogoal.com//wp-content/uploads/2014/08/features.png" alt="features" width="720" height="862"></div><br>
 <div class="flex_column av_two_third   ">
 <div style="height:15px" class="hr hr-invisible "><span class="hr-inner"><span class="hr-inner-style"></span></span></div>
-<div class="flex_column av_one_half first  ">
-<h4 class="ss-icon ss-air ss-dashboard"></h4>
+<div class="col6">
+<img src="/wp-content/uploads/2015/06/dashboard.png">
 <h4>Dashboard</h4>
 <p>All your active challenges are in one place. See what you’re playing when you’re playing. Take part in new challenges and view how you finished in past challenges.</p>
 </div>
-<div class="flex_column av_one_half   ">
-<h4 class="ss-icon ss-air ss-user"></h4>
+<div class="col6">
+<img src="/wp-content/uploads/2015/06/athlete.png">
 <h4>Athlete Stats</h4>
 <p>Mondogoal gives precise statistical information on any player in any league. All the information is broken down into friendly easy-to-digest statistical analysis.</p>
 </div>
 <div style="height:15px" class="hr hr-invisible "><span class="hr-inner"><span class="hr-inner-style"></span></span></div>
-<div class="flex_column av_one_half first  ">
-<h4 class="ss-icon ss-air ss-barchart"></h4>
+<div class="col6">
+<img src="/wp-content/uploads/2015/06/stats.png">
 <h4>Detailed Stats</h4>
 <p>Not interested in fancy graphics? Just looking for hard-core stats? We have you covered. All data on every athlete is available for every Mondogoal user.</p>
 </div>
-<div class="flex_column av_one_half   ">
-<h4 class="ss-icon ss-air ss-notifications"></h4>
+<div class="col6">
+<img src="/wp-content/uploads/2015/06/ring.png">
 <h4>Notifications</h4>
 <p>Minute-by-minute updates on all matches you are playing. Find out in seconds if your favorite player scored, or completely missed that penalty kick.</p>
 </div>
 <div style="height:30px" class="hr hr-invisible "><span class="hr-inner"><span class="hr-inner-style"></span></span></div>
 <div class="flex_column av_one_full first  "><a class="btn btn-green signup" href="https://secure.mondogoal.com/signup">PLAY NOW</a></div>
-</div><p></p>
+</div>
 <div class="flex_column av_one_full first  ">
 <div class="b-back-to-top-dark"><a href="#top">&nbsp;</a></div>
 </div>
