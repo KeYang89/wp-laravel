2d2f822b08b4895263daa5d67139c35a11cab3a8
Ke Yang <key@goretailpro.com>
2015-06-16 18:32:36 -0500

    remove sidebar

--- wp-content/themes/tavisha/sidebar.php
+++ wp-content/themes/tavisha/sidebar.php
@@ -1,25 +1,9 @@
 <?php
 $userApp = getUserApp();
 if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-  <div id="widget-area" class="sidebar column col12" role="complementary">
+  <div id="widget-area" class="col12" role="complementary">
 
       <?php dynamic_sidebar( 'sidebar-1' ); ?>
   </div><!-- .widget-area -->
 
 <?php endif; ?>
-<?php
-// function getToken()
-// {
-//     $ch = curl_init();
-
-//     curl_setopt($ch,CURLOPT_URL, 'http://matchday45.com/team/api/v1/getToken');
-//     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-// //  curl_setopt($ch,CURLOPT_HEADER, false);
-
-//     $output=curl_exec($ch);
-
-//     curl_close($ch);
-//     unset($ch);
-//     return $output;
-// }
-?>
