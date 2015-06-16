b74a4932df36f1748a80d3a6a417f2c158b11b38
Ke Yang <key@goretailpro.com>
2015-06-16 10:15:16 -0500

    homepage design change

--- wp-content/themes/tavisha/new_homepage_matchday.php
+++ wp-content/themes/tavisha/new_homepage_matchday.php
@@ -19,64 +19,13 @@ get_header();
   <div class="main-content-inner" id="top">
     <?php get_template_part('content','slider');?>
     <!-- /.slider-section -->
-<div  style="background-repeat: repeat; background-image: url(/wp-content/uploads/2015/06/grey-bg.jpg); background-attachment: scroll; background-position: top left; background-color: #c1c1c1;padding:20px;">
+    <div class="entry-content">
+        <?php
+        /* translators: %s: Name of current post */
+        the_content();
+        ?>
 
-
-
-<img src="/wp-content/uploads/2015/06/daily-fantasy.png" alt="daily-fantasy" style="min-width:240px;max-width:300px;width:100%;">
-
-
-<div class="col6">
-<h2>DAILY FANTASY</h2>
-<h3>An entire fantasy season in one day!</h3>
-<p class="intro-text">Most fantasy sites lock you into a team for an entire season, Mondogoal’s daily play allows you to play a new “season” every day or week.</p>
-<p>As long as there are two matches happening on one day, you can draft a team and compete. Daily play is an exciting format that gives you the chance to prove your managing prowess practically every day. No more waiting months to see if you win or get a payout.</p>
-
-<p><a class="btn btn-green signup" href="#">PLAY NOW</a></p>
-</div>
-<div class="col12">
-<div class="b-back-to-top-dark"><a href="#top">&nbsp;</a></div>
-</div>
-</div>
-
-
-
-
-<div style="background-color:#E3B013;padding:20px;">
-
-<h2>FEATURES</h2>
-<h3>Stats and info that actually matter to you.</h3>
-<div class="col12"><div style="height:30px" class="hr hr-invisible "><span class="hr-inner"><span class="hr-inner-style"></span></span></div><img src="https://mondogoal.com//wp-content/uploads/2014/08/features.png" alt="features" style="min-width:200px;max-width:250px;width:100%;"></div><br>
-<div class="col12">
-<div style="height:15px" class="hr hr-invisible "><span class="hr-inner"><span class="hr-inner-style"></span></span></div>
-<div class="col6">
-<img src="/wp-content/uploads/2015/06/dashboard.png">
-<h4>Dashboard</h4>
-<p>All your active challenges are in one place. See what you’re playing when you’re playing. Take part in new challenges and view how you finished in past challenges.</p>
-</div>
-<div class="col6">
-<img src="/wp-content/uploads/2015/06/athlete.png">
-<h4>Athlete Stats</h4>
-<p>Mondogoal gives precise statistical information on any player in any league. All the information is broken down into friendly easy-to-digest statistical analysis.</p>
-</div>
-<div style="height:15px" class="hr hr-invisible "><span class="hr-inner"><span class="hr-inner-style"></span></span></div>
-<div class="col6">
-<img src="/wp-content/uploads/2015/06/stats.png">
-<h4>Detailed Stats</h4>
-<p>Not interested in fancy graphics? Just looking for hard-core stats? We have you covered. All data on every athlete is available for every Mondogoal user.</p>
-</div>
-<div class="col6">
-<img src="/wp-content/uploads/2015/06/ring.png">
-<h4>Notifications</h4>
-<p>Minute-by-minute updates on all matches you are playing. Find out in seconds if your favorite player scored, or completely missed that penalty kick.</p>
-</div>
-
-<a class="btn btn-green signup" href="#">PLAY NOW</a></div>
-
-
-<div class="b-back-to-top-dark"><a href="#top">&nbsp;</a></div>
-
-</div>
+         </div><!-- .entry-content -->
 
 </div>
 </div><!-- .main-content -->
