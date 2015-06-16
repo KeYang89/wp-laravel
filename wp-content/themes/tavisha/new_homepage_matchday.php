edea159fa41220c3afcd009e06fca7574689e3d9
Ke Yang <key@goretailpro.com>
2015-06-16 11:29:17 -0500

    add new homepage and modify the template

--- wp-content/themes/tavisha/new_homepage_matchday.php
+++ wp-content/themes/tavisha/new_homepage_matchday.php
@@ -1,34 +1,16 @@
 <?php /* Template Name: New Homepage for Matchday */
 get_header();
 ?>
-<style>
-.b-back-to-top-dark a {
-  background-image: url("/wp-content/uploads/2015/06/b-back-to-top-dark.png");
-  background-repeat: no-repeat;
-  display: block;
-  height: 66px;
-  margin: 0 auto;
-  width: 131px;
-}
-.col6
-{
-  float:left;
-}
-</style>
 <div class="main-content column col9 col8-sm" role="main">
-  <div class="main-content-inner" id="top">
+  <div class="main-content-inner">
     <?php get_template_part('content','slider');?>
     <!-- /.slider-section -->
-    <div class="entry-content">
-        <?php
-        /* translators: %s: Name of current post */
-        the_content();
-        ?>
-
-         </div><!-- .entry-content -->
-
+  </div><!-- .main-content-inner -->
+<div class="how-it-works">
+ <?php the_content();?>
 </div>
 </div><!-- .main-content -->
+
 <?php get_sidebar();?>
 <!-- .sidebar -->
 <?php get_footer(); ?>
