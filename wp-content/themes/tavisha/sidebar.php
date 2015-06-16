30fb360b2f37a5d4aae7e994e089f9164397551c
Ke Yang <key@goretailpro.com>
2015-06-16 18:37:15 -0500

    modify count-down function

--- wp-content/themes/tavisha/sidebar.php
+++ wp-content/themes/tavisha/sidebar.php
@@ -2,7 +2,6 @@
 $userApp = getUserApp();
 if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
   <div id="widget-area" class="col12" role="complementary">
-
       <?php dynamic_sidebar( 'sidebar-1' ); ?>
   </div><!-- .widget-area -->
 
