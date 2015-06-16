5158c405d5e78f903621a6ecf9a6e2f92a0d07dd
Ke Yang <key@goretailpro.com>
2015-06-16 17:46:49 -0500

    add social icons, add the count down block, remove the sidebar

--- wp-content/themes/tavisha/new_homepage_matchday.php
+++ wp-content/themes/tavisha/new_homepage_matchday.php
@@ -1,7 +1,7 @@
 <?php /* Template Name: New Homepage for Matchday */
 get_header();
 ?>
-<div class="main-content column col9 col8-sm" role="main">
+<div class="main-content column col12 col12-sm" role="main">
   <div class="main-content-inner">
     <?php get_template_part('content','slider');?>
     <!-- /.slider-section -->
